69c689cb22c15de23a30210b3f84fb74bb4403e5
Brayan Betancourt Ramirez <67015417+Betancourt1996@users.noreply.github.com>
2022-07-17 21:12:28 -0500

    selector de temporadas

--- Proyecto2/ligas.js
+++ Proyecto2/ligas.js
@@ -1,28 +1,29 @@
-let id_default="arg.1"
+let id_default = "arg.1"
 document.addEventListener('DOMContentLoaded', () => {
     cargarSelect();
     cargarTabla();
     interaccionTabla();
-    
+    cargarSeasons();
 }
 )
 let cargarSelect = () => {
     (async () => {
         try {
             let seleccion = document.getElementById("select");
-            seleccion.innerHTML ="";
+            seleccion.innerHTML = "";
             const response = await fetch("https://api-football-standings.azharimm.site/leagues");
             const data = await response.json();
             ligas = data.data
             let rank = 0
-            ligas.forEach((liga) => {                
-                let id=liga.id
-                let name=liga.name
+            ligas.forEach((liga) => {
+                let id = liga.id
+                let name = liga.name
                 let plantilla = `
                 <option value="${id}" >${name}</option>
                 `
                 seleccion.innerHTML += plantilla;
             });
+
 
         } catch (err) {
             console.error(err);
@@ -32,24 +33,24 @@ let cargarSelect = () => {
 }
 
 
-let cargarTabla = (idLiga=id_default) => {
-    
+let cargarTabla = (idLiga = id_default,anho="2022") => {
+
     (async () => {
         try {
-           
-            
+
+
             let tableBody = document.getElementById("tabla");
-            tableBody.innerHTML ="";
-            
-            const response = await fetch("https://api-football-standings.azharimm.site/leagues/"+idLiga+"/standings?season=2020&sort=asc");
+            tableBody.innerHTML = "";
+
+            const response = await fetch("https://api-football-standings.azharimm.site/leagues/" + idLiga + "/standings?season="+anho+"&sort=asc");
             const data = await response.json();
-            
+
             posiciones = data.data.standings
-            let tableGraf=document.getElementById("grafBarras");
-            tableGraf.innerHTML ="";
+            let tableGraf = document.getElementById("grafBarras");
+            tableGraf.innerHTML = "";
             let rank = 0
             posiciones.forEach((posicione) => {
-                
+
                 let equipo = posicione.team.name;
                 let pj = posicione.stats[3].value
                 let g = posicione.stats[0].value;
@@ -65,20 +66,20 @@ let cargarTabla = (idLiga=id_default) => {
                 </tr>
                 `
                 tableBody.innerHTML += plantilla;
-                
 
-                
-                let perdidos=pj-g
-                let porcGanados=g/pj
-                let porcPerdidos=perdidos/pj
-                let plantillaGraf=`
+
+
+                let perdidos = pj - g
+                let porcGanados = g / pj
+                let porcPerdidos = perdidos / pj
+                let plantillaGraf = `
                 <tr class="table-primary">
                 <td scope="col"  style="--size:${porcGanados}; --color:rgba(38, 193, 0, 0.8)">${g}</th>
                 <td scope="col"  style="--size:${porcPerdidos}; --color:rgba(214, 0, 0, 0.8)">${perdidos}</th>
               </tr>
                 
                 `
-                tableGraf.innerHTML+=plantillaGraf;
+                tableGraf.innerHTML += plantillaGraf;
                 //<td scope="col">${perdidos}</th>
             });
 
@@ -89,15 +90,50 @@ let cargarTabla = (idLiga=id_default) => {
     })();
 }
 
-let interaccionTabla=()=>{
-    let select=  document.getElementById("select");
-    let NombreLiga
-    select.addEventListener('change',()=>{       
-        nameLiga=document.getElementById("NombreLiga");
-        nameLiga.innerText=select.options[select.selectedIndex].textContent
-          cargarTabla(select.value);
-  
+let interaccionTabla = () => {
+    let select = document.getElementById("select");
+    let seasons = document.getElementById("seasons")
+    select.addEventListener('change', () => {
+        nameLiga = document.getElementById("NombreLiga");
+        nameLiga.innerText = select.options[select.selectedIndex].textContent
+        cargarTabla(select.value);
+        cargarSeasons(select.value,seasons.value)
+
+    })
+    seasons.addEventListener('change', () => {
+        nameLiga = document.getElementById("NombreLiga");
+        nameLiga.innerText = select.options[select.selectedIndex].textContent
+        cargarTabla(select.value,seasons.value);
+        cargarSeasons(select.value,seasons.value)
+
     })
 
+}
+
+let cargarSeasons = (nliga="arg.1",temporada="2020") => {
+
+
+    (async () => {
+        try {
+            let seasons = document.getElementById("seasons")
+            seasons.innerHTML = "";
+            const response = await fetch("https://api-football-standings.azharimm.site/leagues/" +nliga+ "/seasons");
+            const data = await response.json();
+            temp = data.data.seasons
+            console.log(temp)
+            temp.forEach((liga) => {
+                
+                anho=liga.year
+                let plantilla = `
+                <option value="${anho}" >${anho}</option>
+                `
+                seasons.innerHTML += plantilla;
+            });
+
+
+        } catch (err) {
+            console.error(err);
+        }
+    })();
 }
 
